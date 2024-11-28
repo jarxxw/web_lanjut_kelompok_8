2aad4a1ba93e48c054e0790bdfe5804478cebc80
jarxxw <sirkeldensenyo@gmail.com>
2024-11-28 12:56:21 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h1><p align="center">WELCOME TO THE YUDISTHIO COMMUNITY </p></h1>
+    </div>
+</body>
+</html>
