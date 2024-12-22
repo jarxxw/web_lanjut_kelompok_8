dbac4ad8cdbac0e76811ab94158cff0b79a21b48
jarxxw <sirkeldensenyo@gmail.com>
2024-12-22 10:23:50 +0700

    Update dosen.php

--- dosen.php
+++ dosen.php
@@ -1,19 +1,17 @@
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Dosen</h1>
-      </div>
 <?php
-include 'koneksi.php';
 $aksi = isset($_GET['aksi']) ? $_GET['aksi'] : 'list';
+
 switch ($aksi) {
     case 'list':
 ?>
 
         <div class="row">
+            <h2>Data Dosen</h2>
             <div class="col-2">
-                <a href="index.php?p=dosen&aksi=input" class="btn btn-primary mb-3">Tambah Dosen</a>
+                <a href="index.html?p=dosen&aksi=input" class="btn btn-primary mb-3"><i class="bi bi-file-plus"></i> Tambah Dosen</a>
             </div>
 
-            <table class="table table-bordered table-striped table-sm" id="example">
+            <table class="table table-border">
                 <tr>
                     <th>No</th>
                     <th>NIP</th>
@@ -26,22 +24,24 @@ switch ($aksi) {
                 </tr>
 
                 <?php
-                $ambil = mysqli_query($db, "SELECT * FROM prodi INNER JOIN dosen ON prodi.id = dosen.prodi_id");
+                include 'koneksi.php';
+                $ambil = mysqli_query($db, "SELECT * FROM dosenn");
                 $no = 1;
                 while ($data = mysqli_fetch_array($ambil)) {
                 ?>
+
                     <tr>
                         <td><?php echo $no ?></td>
 
                         <td><?= $data['nip'] ?></td>
                         <td><?= $data['nama_dosen'] ?></td>
                         <td><?= $data['email'] ?></td>
-                        <td><?= $data['nama_prodi'] ?></td>
+                        <td><?= $data['prodi_id'] ?></td>
                         <td><?= $data['notelp'] ?></td>
                         <td><?= $data['alamat'] ?></td>
                         <td>
-                            <a href="index.php?p=dosen&aksi=edit&id=<?= $data['id'] ?>" class="btn btn-success">Edit</a>
-                            <a href="proses_dosen.php?proses=delete&id=<?= $data['id'] ?>" class="btn btn-warning" onclick="return confirm('Yakin akan menghapus data?')">Hapus</a>
+                            <a href="index.php?p=dosen&aksi=edit&id=<?= $data['id'] ?>" class="btn btn-success"><i class="bi bi-pen-fill"></i> Edit</a>
+                            <a href="proses_dosen.php?proses=delete&id=<?= $data['id'] ?>" class="btn btn-warning" onclick="return confirm('Yakin akan menghapus data?')"><i class="bi bi-trash"></i> Hapus</a>
                         </td>
                     </tr>
                 <?php
@@ -58,6 +58,7 @@ switch ($aksi) {
     ?>
         <div class="row">
             <div class="col-6">
+                <h2>Masukkan data Dosen</h2>
                 <div class="col-6">
                     <a href="index.php?p=dosen" class="btn btn-primary mb-3">Data Dosen</a>
 
@@ -90,30 +91,29 @@ switch ($aksi) {
                         <div class="row mb-3">
                             <label class="col-sm-2 col-form-label">Program Studi</label>
                             <div class="col-sm-10">
-                                <select name="prodi_id" id="">
-                                    <option value="">-Pilih Prodi-</option>
-                                    <?php
-                                        $prodi=mysqli_query($db,"SELECT * FROM prodi");
-                                        while ($data_prodi=mysqli_fetch_array($prodi)) {
-                                            $selected=($data_prodi['id']==$data_dosen['prodi_id']) ? 'selected' : '';
-                                            echo "<option value=".$data_prodi['id'].">".$data_prodi['nama_prodi']."</option>";
-                                        }
-                                    ?>
-                                </select>
+                                <input type="text" class="form-control" name="prodi_id">
                             </div>
                         </div>
+
+
+
+
+
                         <div class="row mb-3">
                             <label class="col-sm-2 col-form-label">Nomor Telepon</label>
                             <div class="col-sm-10">
                                 <input type="number" class="form-control" name="notelp">
                             </div>
                         </div>
+
+
                         <div class="row mb-3">
                             <label class="col-sm-2 col-form-label">Alamat</label>
                             <div class="col-sm-10">
                                 <textarea name="alamat" cols="50" rows="4"></textarea>
                             </div>
                         </div>
+
                         <button type="submit" name="proses" class="btn btn-danger">Proses</button> &nbsp
                         <button type="submit" name="proses" class="btn btn-primary"> Reset</button>
                     </form>
@@ -121,21 +121,26 @@ switch ($aksi) {
             </div>
         </div>
     <?php
+
         break;
     case 'edit':
+        include('koneksi.php');
+
         // Ambil data dosen berdasarkan id
-        $ambil = mysqli_query($db, "SELECT * FROM dosen WHERE id='$_GET[id]'");
+        $ambil = mysqli_query($db, "SELECT * FROM dosenn WHERE id='$_GET[id]'");
         $data_dosen = mysqli_fetch_array($ambil);
     ?>
+
         <div class="row">
             <div class="col-6">
+                <h2>Edit Data Dosen</h2>
                 <div class="col-2">
                     <a href="index.php?p=dosen" class="btn btn-primary mb-3">Data Dosen</a>
                 </div>
                 <table>
                     <form action="proses_dosen.php?proses=update" method="POST">
                         <div class="row mb-3">
-                            <label class="col-sm-2 col-form-label">NIP</label>
+                            <label class="col-sm-2 col-form-label">nip</label>
                             <div class="col-sm-10">
                                 <input type="text" class="form-control" name="nip" value="<?= $data_dosen['nip'] ?>">
                             </div>
@@ -153,18 +158,9 @@ switch ($aksi) {
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label class="col-sm-2 col-form-label">Program Studi</label>
+                            <label class="col-sm-2 col-form-label">Prodi ID</label>
                             <div class="col-sm-10">
-                                <select name="prodi_id" id="">
-                                    <option value="">-Pilih Prodi-</option>
-                                    <?php
-                                        $prodi=mysqli_query($db,"SELECT * FROM prodi");
-                                        while ($data_prodi=mysqli_fetch_array($prodi)) {
-                                            $selected=($data_prodi['id']==$data_dosen['prodi_id']) ? 'selected' : '';
-                                            echo "<option value=".$data_prodi['id']." $selected>".$data_prodi['nama_prodi']."</option>";
-                                        }
-                                    ?>
-                                </select>
+                                <input type="text" class="form-control" name="prodi_id" value="<?= $data_dosen['prodi_id'] ?>">
                             </div>
                         </div>
                         <div class="row mb-3">
