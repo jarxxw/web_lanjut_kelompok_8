ae9a85a53e8e73781eb48d7d4ecb64aa82cdb4bf
jarxxw <sirkeldensenyo@gmail.com>
2024-12-22 10:04:19 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,18 @@
 <?php
-$server="localhost";
-$username="root";
-$password="";
-$database_name="kelompok8";
+$server = "localhost";
+$username = "root";
+$password = "";
+$database_name = "kelompok8";
 
-$koneksi= mysqli_connect($server,$username,$password,$database_name);
+try {
+    // Membuat koneksi menggunakan PDO
+    $koneksi = new PDO("mysql:host=$server;dbname=$database_name", $username, $password);
+    
+    // Set atribut error mode ke exception
+    $koneksi->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Koneksi berhasil!";
+} catch (PDOException $e) {
+    // Menangani error koneksi
+    echo "Koneksi gagal: " . $e->getMessage();
+}
 ?>
